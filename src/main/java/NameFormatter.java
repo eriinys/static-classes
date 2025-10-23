42fa5dc61eaad4fc5642b4f3604642db6c1dc32e
eriinys <eriinys@duck.com>
2025-10-23 14:01:10 -0500

    initial commit

--- src/main/java/NameFormatter.java
+++ src/main/java/NameFormatter.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class NameFormatter {
+
+    private NameFormatter() {
+    }
+
+    public static String format(String firstName, String lastName){
+        return lastName + ", " + firstName;
+    }
+
+    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix){
+        return String.format("%s, %s. %s %s, %s%n", lastName, prefix, firstName, middleName, suffix);
+    }
+
+    public static String format(String fullName){
+        return fullName;
+
+    }
 }
