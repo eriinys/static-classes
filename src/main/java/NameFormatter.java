964bb1d20e0f49501e55c0e5c5fdb02728b3a6ae
eriinys <eriinys@duck.com>
2025-10-23 19:59:33 -0500

    added String array to split different name parts in format fullName

--- src/main/java/NameFormatter.java
+++ src/main/java/NameFormatter.java
@@ -11,8 +11,14 @@ public class NameFormatter {
         return String.format("%s, %s. %s %s, %s", lastName, prefix, firstName, middleName, suffix);
     }
 
-    public static String format(String fullName){
-        return fullName;
+    public static String format(String fullName){ //make sure to prompt user when calling method in specific formatted order
+        String[] parts = fullName.split("\\s+");
+        String prefix = parts[0];
+        String firstName = parts[1];
+        String middleName = parts[2];
+        String lastName = parts[3];
+        String suffix = parts[4];
 
+        return fullName;
     }
 }
